d88757153440f740ccc4ba40f00075480e76a786
chohajin <hajin1742@gmail.com>
2024-12-02 10:53:00 +0900

    push common

--- eatingall/src/Components/Common/Header.js
+++ eatingall/src/Components/Common/Header.js
@@ -1,21 +1,24 @@
 import React from "react";
-import "./Common.css"; // 경로 수정
+import "./Common.css";
 
 export default function Header() {
-    return (
-        <>
-            <header className="header-container">
-                <a className="title">다먹자</a>
-                <div className="search-container"> {/* a 태그 -> div로 변경 */}
-                    <input type="text" placeholder="지역명 또는 음식명을 입력하세요..." />
-                    <button type="submit">검색</button>
-                </div>
-                <div className="login">
-                    <button type="submit">로그인</button>
-                </div>
-
-                
-            </header>
-        </>
-    );
+  return (
+    <>
+      <header className="header-container">
+        <a className="title" href="#home">다먹자</a>
+        <div className="search-container">
+          <input type="text" placeholder="지역명 또는 음식명을 입력하세요..." />
+          <button type="submit">검색</button>
+        </div>
+        <div className="login">
+          <button type="submit">로그인</button>
+        </div>
+      </header>
+      <nav className="nav">
+        <a href="#home">홈</a>
+        <a href="#about">소개</a>
+        <a href="#contact">문의</a>
+      </nav>
+    </>
+  );
 }
